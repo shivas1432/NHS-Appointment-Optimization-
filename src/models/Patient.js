82b13ad512939de571230f3c09acec9a5f3f39b5
shivas1432 <shivashanker7337@gmail.com>
2024-08-21 00:00:00 +0100

    Add NHS API configuration and patient data models

---
+++ src/models/Patient.js
@@ -0,0 +1,22 @@
+﻿// NHS Patient Data Model
+class Patient {
+  constructor(nhsNumber, name, dateOfBirth, address) {
+    this.nhsNumber = nhsNumber;
+    this.name = name;
+    this.dateOfBirth = dateOfBirth;
+    this.address = address;
+    this.appointments = [];
+    this.medicalHistory = [];
+  }
+
+  addAppointment(appointment) {
+    this.appointments.push(appointment);
+  }
+
+  getUpcomingAppointments() {
+    const now = new Date();
+    return this.appointments.filter(apt => new Date(apt.dateTime) > now);
+  }
+}
+
+module.exports = Patient;
